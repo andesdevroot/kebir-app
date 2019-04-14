4cd12fa7169eb8bcce150416a8d0ce146a232b06
andesdevroot <andes.dev.root@gmail.com>
2019-04-14 12:46:58 -0400

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :trucks
   devise_for :users
   resources :camions
-  root to: redirect('/camions')
+  root to: redirect('/trucks')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
