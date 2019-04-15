71d4903d99e5939858856c8d1d68e30a86eb70b6
andesdevroot <andes.dev.root@gmail.com>
2019-04-15 01:48:03 -0400

    update app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_053749) do
+ActiveRecord::Schema.define(version: 2019_04_15_024231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 2019_04_15_053749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "volumen_carga"
-    t.integer "equipos_carga"
   end
 
   create_table "trucks", force: :cascade do |t|
